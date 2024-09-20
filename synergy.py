de51b23105a197711a76d5d172c2dd1d409a22a3
Slx5 <ddubovcev99@mail.ru>
2024-09-20 15:25:40 +0300

    Add files via upload

---
+++ synergy.py
@@ -0,0 +1,20 @@
+# Задание 1
+
+type = input("Введите вид питомца: ")
+age = input("Введите возраст питомца: ")
+name = input("Введите имя питомца: ")
+
+print("Вид питомца:", type)
+print("Возраст питомца:", age)
+print("Имя питомца:", name)
+
+# Задание 2
+
+first_stage = input("Введите первую стадию: ")
+second_stage = input("Введите вторую стадию: ")
+third_stage = input("Введите третью стадию: ")
+fourth_stage = input("Введите четвертую стадию: ")
+fifth_stage = input("Введите пятую стадию: ")
+sixth_stage = input("Введите шестую стадию: ")
+seventh_stage = input("Введите седьмую стадию: ")
+print("Первая стадия", first_stage , "Вторая стадия", second_stage ,"Третья стадия", third_stage ,"Четвертая стадия", fourth_stage ,"Пятая стадия", fifth_stage ,"Шестая стадия", sixth_stage ,"Седьмая стадия", seventh_stage , sep='=>')git init
